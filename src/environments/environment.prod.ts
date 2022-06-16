69f5ac7300299158ba3738e6aeb3862d28e4b179
Yura935 <boyko.yuriy16@gmail.com>
2022-06-16 14:17:13 +0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB5mBaAx9NG0xeUCU0cI2gwu2IfkfIS8MM",
+    authDomain: "clearchat-e1062.firebaseapp.com",
+    projectId: "clearchat-e1062",
+    storageBucket: "clearchat-e1062.appspot.com",
+    messagingSenderId: "797637390498",
+    appId: "1:797637390498:web:8d041c51c88a5bebb3a7fb"
+  }
 };
