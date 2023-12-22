4798b4857c761aceafb5cddebf3a81851e5bd61f
Yura935 <boyko.yuriy16@gmail.com>
2023-12-22 13:18:08 +0200

    Adding Formly package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +20,11 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 
 import { FilterPipe } from './pipes/filter.pipe';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -26,10 +35,45 @@ import { MatButtonModule } from '@angular/material/button';
 import { ClearHistoryModalComponent } from './components/clear-history-modal/clear-history-modal.component';
 import { SettingsModalComponent } from './components/settings-modal/settings-modal.component';
 
-import {DEFAULT_CONFIG, NgForageOptions, NgForageConfig, Driver} from 'ngforage';
+import { DEFAULT_CONFIG, NgForageOptions, Driver } from 'ngforage';
+import { FORMLY_CONFIG, FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, "./assets/i18n/", ".json?cb=" + new Date().getTime());
+  return new TranslateHttpLoader(
+    http,
+    './assets/i18n/',
+    '.json?cb=' + new Date().getTime()
+  );
+}
+
+// bug with checking passwords, deosn't display message when it should
+export function fieldMatchValidator(control: AbstractControl) {
+  const { password, passwordConfirm } = control.value;
+
+  // avoid displaying the message error when values are empty
+  if (!passwordConfirm || !password) {
+    return null;
+  }
+
+  if (passwordConfirm === password) {
+    return null;
+  }
+
+  return { fieldMatch: { message: 'Password Not Matching' } };
+}
+
+export function formlyValidationConfig(translate: TranslateService) {
+  return {
+    validationMessages: [
+      {
+        name: 'required',
+        message() {
+          return translate.stream('auth.validation.required');
+        },
+      },
+    ],
+  };
 }
 
 @NgModule({
@@ -56,26 +100,46 @@ export function HttpLoaderFactory(http: HttpClient) {
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
+        deps: [HttpClient],
       },
-      defaultLanguage: "en"
+      defaultLanguage: 'en',
     }),
     BrowserAnimationsModule,
     NgClickOutsideModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    FormlyModule.forRoot({
+      validators: [
+        { name: 'email', validation: Validators.email },
+        { name: 'fieldMatch', validation: fieldMatchValidator },
+      ],
+      // validationMessages: [
+      //   { name: 'required', message: 'This field is required' },
+      // ],
+    }),
+    FormlyBootstrapModule,
   ],
-  providers: [FilterPipe, {
-    provide: DEFAULT_CONFIG,
-    useValue: {
-      name: 'MyApp',
-      driver: [ // defaults to indexedDB -> webSQL -> localStorage
-        Driver.INDEXED_DB,
-        Driver.LOCAL_STORAGE
-      ]
-    } as NgForageOptions
-  }],
+  providers: [
+    FilterPipe,
+    {
+      provide: DEFAULT_CONFIG,
+      useValue: {
+        name: 'MyApp',
+        driver: [
+          // defaults to indexedDB -> webSQL -> localStorage
+          Driver.INDEXED_DB,
+          Driver.LOCAL_STORAGE,
+        ],
+      } as NgForageOptions,
+    },
+    {
+      provide: FORMLY_CONFIG,
+      multi: true,
+      useFactory: formlyValidationConfig,
+      deps: [TranslateService],
+    },
+  ],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/auth/auth.component.ts
+++ src/app/login/auth/auth.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgForage } from 'ngforage';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { TranslateService } from '@ngx-translate/core';
 import { FontSize } from 'src/app/classes/fontSize.model';
 import { DataService } from 'src/app/services/data.service';
 import { ThemeService } from 'src/app/services/theme.service';
@@ -9,55 +15,221 @@ import { ThemeService } from 'src/app/services/theme.service';
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AuthComponent implements OnInit {
   public isAuth: boolean = true;
-  public authForm: FormGroup;
-  public signInForm: FormGroup;
+  public signUpForm: FormGroup = new FormGroup({});
+  public signInForm: FormGroup = new FormGroup({});
   private regExpPass = /^[A-Za-z\d+=]{6,30}$/gm;
   private size: FontSize = {
     id: 's2',
     title: '1.4em',
     text: '16px',
   };
+  signInModel = {
+    email: '',
+    password: '',
+  };
+  signInOptions: FormlyFormOptions = {};
+  signInFields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        // label: 'Email address',
+        type: 'email',
+        // placeholder: 'Enter email',
+        required: true,
+      },
+      validators: {
+        validation: [Validators.email],
+      },
+      validation: {
+        messages: {
+          email: (error, field: FormlyFieldConfig) =>
+            `"${field.formControl.value}" is not a valid email`,
+        },
+      },
+      expressionProperties: {
+        'templateOptions.label': this.translate.stream('auth.email'),
+        'templateOptions.placeholder': this.translate.stream(
+          'auth.emailPlaceholder'
+        ),
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      templateOptions: {
+        // label: 'Password',
+        type: 'password',
+        // placeholder: 'Enter password',
+        required: true,
+        // pattern: this.regExpPass,
+        minLength: 6,
+      },
+      validation: {
+        messages: {
+          pattern: (error, field: FormlyFieldConfig) => `Invalid password`,
+          minLength: (error, field: FormlyFieldConfig) =>
+            `Should have at least ${field.templateOptions.minLength} characters`,
+        },
+      },
+      expressionProperties: {
+        'templateOptions.label': this.translate.stream('auth.password'),
+        'templateOptions.placeholder': this.translate.stream(
+          'auth.passwordPlaceholder'
+        ),
+      },
+    },
+  ];
+  signUpModel = {
+    userName: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  };
+  signUpOptions: FormlyFormOptions = {};
+  signUpFields: FormlyFieldConfig[] = [
+    {
+      key: 'userName',
+      type: 'input',
+      templateOptions: {
+        label: 'Username',
+        placeholder: 'Enter username',
+        required: true,
+        min: 5,
+      },
+      validation: {
+        messages: {
+          min: (error, field: FormlyFieldConfig) =>
+            `Should have at least ${field.templateOptions.min} characters`,
+        },
+      },
+    },
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        // label: 'Email address',
+        type: 'email',
+        // placeholder: 'Enter email',
+        required: true,
+      },
+      validators: {
+        validation: ['email'],
+      },
+      validation: {
+        messages: {
+          email: (error, field: FormlyFieldConfig) => {
+            console.log(field);
+            return `"${field.formControl.value}" is not a valid email`;
+          },
+        },
+      },
+      asyncValidators: {
+        uniqueUsername: {
+          expression: (control: FormControl) => {
+            return new Promise((resolve, reject) => {
+              resolve(control.value !== 'user@gmail.com');
+            });
+          },
+          message: 'This email is already taken.',
+        },
+      },
+      expressionProperties: {
+        'templateOptions.label': this.translate.stream('auth.email'),
+        'templateOptions.placeholder': this.translate.stream(
+          'auth.emailPlaceholder'
+        ),
+      },
+    },
+    {
+      validators: {
+        validation: [
+          { name: 'fieldMatch', options: { errorPath: 'passwordConfirm' } },
+        ],
+      },
+      fieldGroup: [
+        {
+          key: 'password',
+          type: 'input',
+          templateOptions: {
+            // label: 'Password',
+            type: 'password',
+            // placeholder: 'Enter password',
+            required: true,
+            pattern: this.regExpPass,
+            min: 6,
+          },
+          validation: {
+            messages: {
+              pattern: (error, field: FormlyFieldConfig) => `Invalid password`,
+              minLength: (error, field: FormlyFieldConfig) =>
+                `Should have at least ${field.templateOptions.minLength} characters`,
+            },
+          },
+          expressionProperties: {
+            'templateOptions.label': this.translate.stream('auth.password'),
+            'templateOptions.placeholder': this.translate.stream(
+              'auth.passwordPlaceholder'
+            ),
+          },
+        },
+        {
+          key: 'passwordConfirm',
+          type: 'input',
+          templateOptions: {
+            // label: 'Confirm Password',
+            type: 'password',
+            // placeholder: 'Please re-enter your password',
+            required: true,
+            pattern: this.regExpPass,
+          },
+          expressionProperties: {
+            'templateOptions.label': this.translate.stream(
+              'auth.confirmPassword'
+            ),
+            'templateOptions.placeholder': this.translate.stream(
+              'auth.confirmPasswordPlaceholder'
+            ),
+          },
+        },
+      ],
+    },
+  ];
 
   constructor(
     private fb: FormBuilder,
     private dataService: DataService,
     private themeService: ThemeService,
-    private readonly ngf: NgForage
-  ) {}
+    public translate: TranslateService
+  ) {
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+
+    translate.use(browserLang.match(/en|ua/) ? browserLang : 'en');
+  }
 
   ngOnInit(): void {
-    this.authForm = this.fb.group({
-      userName: this.fb.control('', [Validators.required]),
-      email: this.fb.control('', [Validators.required, Validators.email]),
-      password: this.fb.control('', [
-        Validators.required,
-        Validators.pattern(this.regExpPass),
-      ]),
-    });
-    this.signInForm = this.fb.group({
-      email: this.fb.control('', [Validators.required, Validators.email]),
-      password: this.fb.control('', [
-        Validators.required,
-        ,
-        Validators.pattern(this.regExpPass),
-      ]),
-    });
     localStorage.removeItem('contact');
   }
 
   signUp(): void {
-    this.dataService.signUp(this.authForm.value);
-    localStorage.setItem('theme', JSON.stringify(this.themeService.light));
-    localStorage.setItem('fontSize', JSON.stringify(this.size));
+    if (this.signUpForm.valid) {
+      this.dataService.signUp(this.signUpForm.value);
+      localStorage.setItem('theme', JSON.stringify(this.themeService.light));
+      localStorage.setItem('fontSize', JSON.stringify(this.size));
+    }
   }
 
   signIn(): void {
-    this.dataService.signIn(this.signInForm.value);
-    localStorage.setItem('theme', JSON.stringify(this.themeService.light));
-    localStorage.setItem('fontSize', JSON.stringify(this.size));
+    if (this.signInForm.valid) {
+      this.dataService.signIn(this.signInForm.value);
+      localStorage.setItem('theme', JSON.stringify(this.themeService.light));
+      localStorage.setItem('fontSize', JSON.stringify(this.size));
+    }
   }
 
   changeChoice(): void {
